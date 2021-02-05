2c37614be66e9b47d35c2ae8eb8eabe28cbc673d
Sumeet Singh <sumeet605@gmail.com>
2021-02-04 19:05:57 -0500

    simulation edits

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -49,6 +52,9 @@ public class Robot extends TimedRobot {
     private static final String kSixBallAuto = "Six Ball Auto";
     private String m_autoSelected;
     private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+    private final Field2d m_field = new Field2d();
+    private final DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(new Rotation2d());
 
     Joystick rightJoystick = new Joystick(0);
     Joystick leftJoystick = new Joystick(1);
@@ -73,12 +79,9 @@ public class Robot extends TimedRobot {
     Servo hoodServo = new Servo(0);
     Spark RGBController = new Spark(1);
 
-    /* 
-    10 - right shooter
-    11 - left shooter
-    12 - intake
-    13 - feeder
-    14 - tower */
+    /*
+     * 10 - right shooter 11 - left shooter 12 - intake 13 - feeder 14 - tower
+     */
 
     CANSparkMax sparkMFlywheelNeo = new CANSparkMax(10, MotorType.kBrushless);
     CANSparkMax sparkMFlywheelNeo1 = new CANSparkMax(11, MotorType.kBrushless);
@@ -90,22 +93,29 @@ public class Robot extends TimedRobot {
     CANPIDController flywheelPIDController;
     double turretPos = srxTurret775.getSelectedSensorPosition();
     Timer autoTimer = new Timer();
-    
+
     int flywheelRPM = 3800;
 
     ClimberFinalCode theClimber = new ClimberFinalCode(theController, srxClimber775_0, srxClimber775_1);
-    FinalDriveTrainCode theDriveTrain = new FinalDriveTrainCode(falcon1, falcon2, falcon3, falcon4, leftJoystick, rightJoystick);
-    FinalIntakeCode theIntake = new FinalIntakeCode(theController, thePiston, sparkMIntakeNeo, sparkMTowerNeo, theCompressor, sparkMFeederNeo);
-    //TalonMusic talonMusic = new TalonMusic(falcon1, falcon2, falcon3, falcon4, "StarWarsThroneRoom.chrp", rightJoystick);
+    FinalDriveTrainCode theDriveTrain = new FinalDriveTrainCode(falcon1, falcon2, falcon3, falcon4, leftJoystick,
+            rightJoystick, theController);
+    FinalIntakeCode theIntake = new FinalIntakeCode(theController, thePiston, sparkMIntakeNeo, sparkMTowerNeo,
+            theCompressor, sparkMFeederNeo);
+    // TalonMusic talonMusic = new TalonMusic(falcon1, falcon2, falcon3, falcon4,
+    // "StarWarsThroneRoom.chrp", rightJoystick);
     Feeder theFeeder = new Feeder(theController, sparkMFeederNeo, sparkMTowerNeo, opticalSensor);
-    Shooter theManualShooter = new Shooter(hoodServo, srxTurret775, flywheelEncoder, flywheelPIDController, sparkMFlywheelNeo, theController, flywheelRPM, .8, turretPos);
-    FinalAutonomousCode autoCode = new FinalAutonomousCode(falcon1, falcon2, falcon3, falcon4, sparkMFlywheelNeo, sparkMFeederNeo, sparkMTowerNeo, sparkMIntakeNeo, thePiston, autoTimer, flywheelEncoder, flywheelPIDController, flywheelRPM, hoodServo, sparkMFlywheelNeo, srxTurret775, theController, turretPos, RGBController);
-    AutoShooterController theAutoShooterController = new AutoShooterController(hoodServo, sparkMFlywheelNeo, srxTurret775, theController, turretPos, flywheelPIDController, flywheelEncoder, flywheelRPM, RGBController);
+    Shooter theManualShooter = new Shooter(hoodServo, srxTurret775, flywheelEncoder, flywheelPIDController,
+            sparkMFlywheelNeo, theController, flywheelRPM, .8, turretPos);
+    FinalAutonomousCode autoCode = new FinalAutonomousCode(falcon1, falcon2, falcon3, falcon4, sparkMFlywheelNeo,
+            sparkMFeederNeo, sparkMTowerNeo, sparkMIntakeNeo, thePiston, autoTimer, flywheelEncoder,
+            flywheelPIDController, flywheelRPM, hoodServo, sparkMFlywheelNeo, srxTurret775, theController, turretPos,
+            RGBController);
+    AutoShooterController theAutoShooterController = new AutoShooterController(hoodServo, sparkMFlywheelNeo,
+            srxTurret775, theController, turretPos, flywheelPIDController, flywheelEncoder, flywheelRPM, RGBController);
 
-    
     /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
      */
     @Override
     public void robotInit() {
@@ -122,7 +132,7 @@ public class Robot extends TimedRobot {
         srxClimber775_1.setInverted(true);
 
         hoodServo.setAngle(0);
-        theIntake.startCompressor();    
+        theIntake.startCompressor();
 
         srxTurret775.configSelectedFeedbackSensor(FeedbackDevice.Analog, 0, 1);
         srxTurret775.set(ControlMode.Position, 502);
@@ -131,27 +141,31 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * This function is called every robot packet, no matter the mode. Use
-     * this for items like diagnostics that you want ran during disabled,
-     * autonomous, teleoperated and test.
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
      *
-     * <p>This runs after the mode specific periodic functions, but before
-     * LiveWindow and SmartDashboard integrated updating.
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
      */
     @Override
     public void robotPeriodic() {
+        SmartDashboard.putData("Field", m_field);
+        m_field.setRobotPose(m_odometry.getPoseMeters());
     }
 
     /**
      * This autonomous (along with the chooser code above) shows how to select
-     * between different autonomous modes using the dashboard. The sendable
-     * chooser code works with the Java SmartDashboard. If you prefer the
-     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-     * getString line to get the auto name from the text box below the Gyro
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString line to get the
+     * auto name from the text box below the Gyro
      *
-     * <p>You can add additional auto modes by adding additional comparisons to
-     * the switch structure below with additional strings. If using the
-     * SendableChooser make sure to add them to the chooser code above as well.
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure below with additional strings. If using the SendableChooser
+     * make sure to add them to the chooser code above as well.
      */
     @Override
     public void autonomousInit() {
@@ -167,21 +181,21 @@ public class Robot extends TimedRobot {
     public void autonomousPeriodic() {
         switch (m_autoSelected) {
             case kMoveFowardAuto:
-            autoCode.AutonomousCodeTwo();
-            break;
+                autoCode.AutonomousCodeTwo();
+                break;
             case kMoveShootAuto:
-            autoCode.AutonomousCodeThree();
-            break;
+                autoCode.AutonomousCodeThree();
+                break;
             case kMoveShootIntake:
-            autoCode.AutonomousCodeFive();
-            break;
+                autoCode.AutonomousCodeFive();
+                break;
             case kSixBallAuto:
-            autoCode.SixBallAuto();
-            break;
+                autoCode.SixBallAuto();
+                break;
             case kDefaultAuto:
             default:
-            autoCode.AutonomousCodeOne();
-            break;
+                autoCode.AutonomousCodeOne();
+                break;
         }
     }
 
@@ -189,19 +203,19 @@ public class Robot extends TimedRobot {
      * This function is called periodically during operator control.
      */
 
-    //boolean isDriving = false;
+    // boolean isDriving = false;
 
     @Override
     public void teleopPeriodic() {
 
-        //turret mid ----    502
-        //limelight sees whole thing max - 6'7" -, stops at 5'
+        // turret mid ---- 502
+        // limelight sees whole thing max - 6'7" -, stops at 5'
 
         theClimber.RunClimber();
         theIntake.PowerCellArmIntake();
-        //theIntake.PowerCellRollerIntake();
+        // theIntake.PowerCellRollerIntake();
         theFeeder.FeederController();
-        //theManualShooter.FlywheelSimple();
+        // theManualShooter.FlywheelSimple();
         theManualShooter.ManualControl();
         theDriveTrain.DriveTrain();
         theAutoShooterController.shooterController();
@@ -222,9 +236,9 @@ public class Robot extends TimedRobot {
 
         pipe.setValue(1);
 
-        if(theController.getTriggerAxis(Hand.kRight) > .1){
+        if (theController.getTriggerAxis(Hand.kRight) > .1) {
             srxClimber775_0.set(ControlMode.PercentOutput, theController.getTriggerAxis(Hand.kRight));
-        }else{
+        } else {
             srxClimber775_0.set(ControlMode.PercentOutput, 0);
         }
 
